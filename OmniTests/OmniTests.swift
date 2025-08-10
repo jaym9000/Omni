77bbe3f43cfa103ca946a3a7e915650bfc4d1f36
jm <your-email@example.com>
2025-08-10 13:05:03 -0300

    Initial Commit

---
+++ OmniTests/OmniTests.swift
@@ -0,0 +1,16 @@
+//
+//  OmniTests.swift
+//  OmniTests
+//
+//  Created by JM Mahoro on 2025-08-10.
+//
+
+import Testing
+
+struct OmniTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
